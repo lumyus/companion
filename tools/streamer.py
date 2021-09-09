7aa88d9000906c07428dd18a403ff4e8057a0051
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-09 17:03:15 +1000

    streamer: allow specifying separate vidformat and param files

--- tools/streamer.py
+++ tools/streamer.py
@@ -10,10 +10,13 @@ os.system("sudo modprobe bcm2835-v4l2")
 while True:
   while (os.system("ls /dev/video* 2>/dev/null") != 0) or (os.path.isfile(home + "/companion/start_video.sh")):
     sleep(5)
-  if len(sys.argv) == 1:
+  arg_count = len(sys.argv)
+  if arg_count == 1:
     params = "$(cat /home/pi/vidformat.param)"
-  elif len(sys.argv) == 2:
+  elif arg_count == 2:
     params = "$(cat /home/pi/{0})".format(sys.argv[1])
+  elif arg_count == 3:
+    params = "$(cat /home/pi/{0}) {1}".format(sys.argv[1], sys.argv[2])
   else:
     params = " ".join(sys.argv[1:]))
   os.system(home + "/companion/scripts/start_video.sh " + params
