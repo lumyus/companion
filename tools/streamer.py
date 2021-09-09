ab462ef6bd7d62fb0b362675b4cd124bb3f8107c
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-09 16:55:03 +1000

    streamer: allow specifying params file

--- tools/streamer.py
+++ tools/streamer.py
@@ -11,9 +11,12 @@ while True:
   while (os.system("ls /dev/video* 2>/dev/null") != 0) or (os.path.isfile(home + "/companion/start_video.sh")):
     sleep(5)
   if len(sys.argv) == 1:
-    os.system(home + "/companion/scripts/start_video.sh $(cat /home/pi/vidformat.param)")
+    params = "$(cat /home/pi/vidformat.param)"
+  elif len(sys.argv) == 2:
+    params = "$(cat /home/pi/{0})".format(sys.argv[1])
   else:
-    os.system(home + "/companion/scripts/start_video.sh " + " ".join(sys.argv[1:]))
+    params = " ".join(sys.argv[1:]))
+  os.system(home + "/companion/scripts/start_video.sh " + params
   sleep(2)
 
 
